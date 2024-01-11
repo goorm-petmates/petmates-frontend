d528b5e79ed9ea0e281e185d175cf4f72a8bd0b7
Jiyeong <tokyru@naver.com>
2024-01-11 10:54:08 +0900

    페이지 기본 틀 구성

---
+++ src/pages/main/Basic.js
@@ -0,0 +1,21 @@
+import "../styles/StyleBasic.css";
+import React from 'react';
+
+function Basic() {
+  return (
+    <div className="element">
+      <div className="asideleft"/>
+      <div className="sesection"/>
+      <div className="asideright"/>
+      <img className="logo" alt="Logo" src="/img/Logo.png"/>
+      <div className="logintext">로그인</div>
+      <button className="headerButtons petsiter">펫시터</button>
+      <button className="headerButtons community">커뮤니티</button>
+      <button className="headerButtons mypage">마이페이지</button>
+      <button className="customerService">고객센터</button>
+      <div className="copyright">2024</div>
+    </div>
+  );
+}
+
+export default Basic;
