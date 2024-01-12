7867c561a26f9c27c62f3b618a528801957a3147
Jiyeong <tokyru@naver.com>
2024-01-12 12:25:46 +0900

    LeftAside 컴포넌트 추가

---
+++ src/components/LeftAside.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/StyleLeftAside.css";
+function LeftAside() {
+
+  return (
+    <div className="element">
+     <aside className="left-aside"></aside>
+    </div>
+  );
+}
+
+export default LeftAside;
