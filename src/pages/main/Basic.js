741eccf1fcb3d50b745fdf72bb16ea7bad9b04e0
Jiyeong <tokyru@naver.com>
2024-01-12 12:24:04 +0900

    Header, LeftAside, RightAside 컴포넌트 추가

--- src/pages/main/Basic.js
+++ src/pages/main/Basic.js
@@ -1,19 +1,15 @@
 import "../../styles/StyleBasic.css";
 import React from 'react';
+import Header from '../../components/Header';
+import LeftAside from '../../components/LeftAside';
+import RightAside from '../../components/RightAside';
 
 function Basic() {
   return (
     <div className="element">
-      <div className="asideleft"/>
-      <div className="sesection"/>
-      <div className="asideright"/>
-      <img className="logo" alt="Logo" src="/imgs/Logo.png"/>
-      <div className="logintext">로그인</div>
-      <button className="headerButtons petsiter">펫시터</button>
-      <button className="headerButtons community">커뮤니티</button>
-      <button className="headerButtons mypage">마이페이지</button>
-      <button className="customerService">고객센터</button>
-      <div className="copyright">2024</div>
+      <Header/>
+      <LeftAside/>
+      <RightAside/>
     </div>
   );
 }
