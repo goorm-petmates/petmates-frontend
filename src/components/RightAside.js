945365559796ef7c9bc0fc8d38f711605817b16c
Jiyeong <tokyru@naver.com>
2024-01-12 12:26:18 +0900

    RightAside 컴포넌트 추가

---
+++ src/components/RightAside.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/StyleRightAside.css";
+function RightAside() {
+
+  return (
+    <div className="element">
+     <aside className="right-aside"></aside>
+    </div>
+  );
+}
+
+export default RightAside;
