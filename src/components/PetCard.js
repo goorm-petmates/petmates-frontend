4109288ca7ffa7a5d620527b48ca68427e7dbab9
Jiyeong <tokyru@naver.com>
2024-01-18 16:29:18 +0900

    petcard를 공통 컴토넌트에 추가

---
+++ src/components/PetCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../styles/PetCard.css";
+
+function PetCard(props){
+  const { petImgSrc, petInfo } = props;
+
+  return(
+    <div className="pet-card-container">
+      <div className="pet-card">
+        <img className="pet-img" src={petImgSrc} alt="pet-img"/>
+        <div className="pet-info">
+          {petInfo}
+        </div>
+        <div className="pet-card-buttons">
+          <button className="pet-card-button-delete">삭제하기</button>
+          <button className="pet-card-button-edit">수정하기</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PetCard;
