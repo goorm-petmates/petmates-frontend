7793bae249bcc9dd1b04046549af2cc7075b58a9
Jiyeong <tokyru@naver.com>
2024-01-18 16:35:30 +0900

    main css 수정을 위한 구조 변화

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -3,11 +3,13 @@ import "../../styles/StyleMain.css";
 import Header from "../../components/Header";
 import LeftAside from "../../components/LeftAside";
 import RightAside from "../../components/RightAside";
-import MainMenuPetsitter from "./MainMenuPetsitter";
+import { MainMenuPetsitter } from "./MainMenuPetsitter";
+import Nav from "../../components/Nav";
 function Main() {
   return (
     <div className="element">
       <Header />
+      <Nav/>
       <LeftAside />
       <RightAside />
 
@@ -15,18 +17,24 @@ function Main() {
         <div className="main-menu">
           <button className="menu-icons left-icon">&lt;</button>
           <button className="menu-icons right-icon">&gt;</button>
-          <div className="menu-title">펫시터</div>
-          <div className="menu-text">css check</div>
-          <div className="menu-list">
-            <div className="list">1/3</div>
+          <div className="menu-container">
+            <div className="menu-title">펫시터</div>
+            <div className="menu-text">css check</div>
+            <div className="menu-list">
+              <div className="list">1/3</div>
+            </div>
           </div>
+          <img className="list-img" alt="Dog" src="/imgs/dog3.jpeg" />
         </div>
-        <img className="list-img" alt="Dog" src="/imgs/dog3.jpeg" />
       </div>
 
       <div className="sesection">
-        <MainMenuPetsitter className="MainMenuPetsitter1" />
-        <MainMenuPetsitter className="MainMenuPetsitter2" />
+        <div className="main-petsitter-components">
+          <MainMenuPetsitter text="PETMATES로 등록하시고 마음을 나누세요!" />
+          <MainMenuPetsitter text="펫시터 경력 40년차!" />
+          <MainMenuPetsitter text="PETMATES로 선택한 환경에서 편안하게 맡겨주세요 !" />
+          <MainMenuPetsitter text="제일가는 펫시터" />
+        </div>
       </div>
     </div>
   );
