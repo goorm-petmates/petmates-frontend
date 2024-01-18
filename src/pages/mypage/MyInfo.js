67bd380edfae41889a958ab88a05e11f89851227
Jiyeong <tokyru@naver.com>
2024-01-18 16:39:07 +0900

    mypage에서 myinfo로 파일 이름 변경

---
+++ src/pages/mypage/MyInfo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../../components/Header';
+import LeftAside from '../../components/LeftAside';
+import RightAside from '../../components/RightAside';
+
+const MyInfo = () => {
+  return (
+    <div>
+      <Header/>
+      <LeftAside/>
+      <RightAside/>
+
+
+    </div>
+  );
+};
+
+export default MyInfo;
