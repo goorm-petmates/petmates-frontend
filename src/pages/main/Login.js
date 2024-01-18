e45f09098f586353f923e38b74bfdaa13d04010d
Jiyeong <tokyru@naver.com>
2024-01-18 16:31:09 +0900

    login css 수정을 위해 div 추가

--- src/pages/main/Login.js
+++ src/pages/main/Login.js
@@ -2,21 +2,24 @@ import '../../styles/StyleLogin.css';
 import Header from "../../components/Header";
 import LeftAside from "../../components/LeftAside";
 import RightAside from "../../components/RightAside";
-
+import Nav from "../../components/Nav";
 function Login() {
   return (
     <div>
       <Header />
+      <Nav/>
       <LeftAside />
       <RightAside />
 
-      <img className="login-logo" alt="Logo" src="/imgs/Logo.png" />
-      <p className="login-info">
-        서비스 이용을 위해 로그인 해주세요 :)
-      </p>
-      <button className="kakao-button">
-        <img src="/imgs/Logo.png" alt="kakao icon" />
-      </button>
+      <div className="login">
+        <img className="login-logo" alt="Logo" src="/imgs/Logo.png" />
+        <p className="login-info">
+          서비스 이용을 위해 로그인 해주세요 :)
+        </p>
+        <button className="kakao-button">
+          <img src="/imgs/Logo.png" alt="kakao icon" />
+        </button>
+      </div>
     </div>
   );
 }
