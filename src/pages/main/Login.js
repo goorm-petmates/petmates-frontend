f31d46eb97ba8428d7389a0be4645337ef266dad
Jiyeong <tokyru@naver.com>
2024-01-22 14:32:22 +0900

    Login css 수정 및 카카오 로그인 추가

--- src/pages/main/Login.js
+++ src/pages/main/Login.js
@@ -4,6 +4,9 @@ import LeftAside from "../../components/LeftAside";
 import RightAside from "../../components/RightAside";
 import Nav from "../../components/Nav";
 function Login() {
+  const REST_API_KEY='2a0545dc2cd35dfd52e96098d3ef9162'
+  const REDIRECT_URI = 'http://localhost:3000/kakao/callback'
+
   return (
     <div className="element">
       <Header />
@@ -17,7 +20,10 @@ function Login() {
           서비스 이용을 위해 로그인 해주세요 :)
         </p>
         <button className="kakao-button">
-          <img src="/imgs/kakaoLogin.png" alt="kakao icon" />
+          <a
+            href={`https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`}>REST
+            <img src="/imgs/kakaoLogin.png" alt="kakao icon" />
+          </a>
         </button>
       </div>
     </div>
