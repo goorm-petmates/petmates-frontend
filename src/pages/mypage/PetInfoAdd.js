1eed7a5bab48d9b350cc722996682d87072d0ace
Jiyeong <tokyru@naver.com>
2024-01-23 17:46:21 +0900

    펫 정보 등록 페이지 구현

--- src/pages/mypage/PetInfoAdd.js
+++ src/pages/mypage/PetInfoAdd.js
@@ -6,15 +6,13 @@ import Footer from '../../components/Footer';
 function PetInfoAdd() {
 
   return (
-    <div className="pet-info-add-element">
-      <HeaderWithNav/>
+    <div>
+      <HeaderWithNav />
 
       <div>
-        <div className="mypage-bar"/>
+        <div className="mypage-bar" />
         <div className="mypage-bar-text">내정보</div>
       </div>
-
-      <div className="mypage-navunderLine"></div>
 
       <Link to='/myinfo'>
         <button className="mypage-petinfo-nav1">내정보</button>
@@ -23,20 +21,122 @@ function PetInfoAdd() {
         <button className="mypage-petinfo-nav2">반려동물 정보</button>
       </Link>
 
+      <div className="mypage-navunderLine"></div>
+
+
       <div className="pet-info-add-container">
-        <div className="pet-info-add-picture"></div>
-        <div className="pet-info-add-inputs">
-
+        <div>
+          <img className="pet-info-add-picture"
+               src="/imgs/Logo-Icon.png" alt="기본 로고" />
+          <button className="pet-info-add-picture-button">사진 추가</button>
         </div>
-        <Link to='/petInfo'
-              style={{ textDecoration: 'none', color: 'white' }}>
-          <button className="pet-info-add-button">등록하기</button>
-        </Link>
 
+        <div className="pet-info-add-inputs">
+          <div className="pet-info-add-input">
+            <label>이름</label>
+            <input type="text"
+                   placeholder="똑바로" />
+          </div>
 
+          <div className="pet-info-add-input">
+            <label>견종</label>
+            <input type="text"
+                   placeholder="푸들" />
+          </div>
 
+          <div className="pet-info-add-input">
+            <label>나이(출생년도)</label>
+            <div className="pet-info-row">
+              <input type="text"
+                     placeholder="2012" />
+              <span>년도</span>
+            </div>
+          </div>
+
+          <div className="pet-info-add-input">
+            <label>몸무게</label>
+            <div className="pet-info-row">
+              <input type="text"
+                     placeholder="5" />
+              <span>Kg</span>
+            </div>
+          </div>
+
+          <div className="pet-info-add-radio">
+            <label>성별</label>
+            <div className="pet-info-row">
+              <label>
+                <input type="radio" />
+                남
+              </label>
+              <label>
+                <input type="radio" />
+                여
+              </label>
+            </div>
+          </div>
+
+          <div className="pet-info-add-radio">
+            <label>중성화</label>
+            <div className="pet-info-row">
+              <label>
+                <input type="radio" />
+                예
+              </label>
+              <label>
+                <input type="radio" />
+                아니오
+              </label>
+            </div>
+          </div>
+
+          <div className="pet-info-add-radio">
+            <label>알러지</label>
+            <div className="pet-info-row">
+              <label>
+                <input type="radio" />
+                있음
+              </label>
+              <label>
+                <input type="radio" />
+                없음
+              </label>
+            </div>
+          </div>
+
+          <div className="pet-info-add-radio">
+            <label>질병(피부병 등)</label>
+            <div className="pet-info-row">
+              <label>
+                <input type="radio" />
+                있음
+              </label>
+              <label>
+                <input type="radio" />
+                없음
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="pet-info-add-area">
+          <label>참고사항</label>
+          <span>펫시터에게 전달되어야할 내용을 상세히 적어주세요 :)</span>
+          <input type="textarea"
+                 placeholder="ex) 식사횟수 및 배급량, 약복용법,
+                              산책 시 주의사항 및 기타 특이사항
+                              알러지 있는 경우: 어떤 음식 알러지" />
+        </div>
+
+        <div className="pet-info-add-button-container">
+          <Link to="/petInfo"
+                style={{ textDecoration: 'none', color: 'white' }}>
+            <button className="pet-info-add-button">등록하기</button>
+          </Link>
+        </div>
       </div>
-      <Footer/>
+
+      <Footer />
     </div>
   );
 }
