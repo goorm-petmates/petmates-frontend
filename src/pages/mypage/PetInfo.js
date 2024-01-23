c341a0818b21ad4de3f51d871c9d96b6ee09bd2c
Jiyeong <tokyru@naver.com>
2024-01-23 16:20:04 +0900

    css 수정

--- src/pages/mypage/PetInfo.js
+++ src/pages/mypage/PetInfo.js
@@ -9,38 +9,40 @@ import Footer from '../../components/Footer';
 function PetInfo() {
 
   return (
-    <div className="element">
-      <HeaderWithNav/>
+    <div>
+      <HeaderWithNav />
 
       <div>
-        <div className="mypage-bar"/>
+        <div className="mypage-bar" />
         <div className="mypage-bar-text">내정보</div>
       </div>
 
-      <div className="mypage-navunderLine"></div>
-
       <Link to='/myinfo'>
-        <button className="mypage-petinfo-nav1">내정보</button>
+        <button className="petinfo-nav1">내정보</button>
       </Link>
       <Link to='/petInfo'>
-        <button className="mypage-petinfo-nav2">반려동물 정보</button>
+        <button className="petinfo-nav2">반려동물 정보</button>
       </Link>
+      <div className="mypage-navunderLine"></div>
 
-      <button className="mypage-petinfo-add">
-        <Link to='/petInfoadd'
-              style={{ textDecoration: 'none', color: 'white' }}>
-          등록하기
-        </Link>
-      </button>
+      <div className="petinfo-container">
+        <button className="mypage-petinfo-add">
+          <Link to='/petInfoadd'
+                style={{ textDecoration: 'none', color: 'white' }}>
+            등록하기
+          </Link>
+        </button>
 
-      <div className="pet-card-components">
-        <NoContents text="반려동물 정보" ></NoContents>
-        <PetCard petImgSrc="/imgs/dog3.jpeg"
-                 petInfo="똑바로/토이푸들/여아/12살/5kg"/>
-        <PetCard petImgSrc="/imgs/dog3.jpeg"
-                 petInfo="아지/말티즈/여아/12살/5kg"/>
+        <div className="pet-card-components">
+          <NoContents text="반려동물 정보"></NoContents>
+          <PetCard petImgSrc="/imgs/dog3.jpeg"
+                   petInfo="똑바로/토이푸들/여아/12살/5kg" />
+          <PetCard petImgSrc="/imgs/dog3.jpeg"
+                  petInfo="아지/말티즈/여아/12살/5kg" />
+        </div>
       </div>
-      <Footer/>
+
+      <Footer />
     </div>
   );
 }
