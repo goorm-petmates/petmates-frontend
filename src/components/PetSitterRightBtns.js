9f3cf4e5f4579c5d36d7e5b1fac86876c444e6af
Hyejung Na <hyejung.na01@gmail.com>
2024-01-26 18:26:23 +0900

    import오류로 공동컴포넌트 파일 재생성

---
+++ src/components/PetSitterRightBtns.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaRegPenToSquare } from 'react-icons/fa6';
+import '../styles/StylePetSitterRightBtns.css';
+
+export const PetSitterRightbtns = () => {
+  return (
+    <div>
+      <div className='petsitter-right-bar'>
+        <button type='button' className='apply-btn'>
+          <Link to='/petsitterfoam'>
+            <FaRegPenToSquare />
+            펫시터 지원하기
+          </Link>
+        </button>
+        <button type='button' className='guide-btn'>
+          <Link to='/petsitterguide'>
+            <FaRegPenToSquare />
+            이용안내
+          </Link>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PetSitterRightbtns;
