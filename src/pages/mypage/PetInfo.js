99fffd207264bcb2d8580f9b1656fbe831274a97
Jiyeong <tokyru@naver.com>
2024-02-07 18:49:10 +0900

    data.pet 값에 따라 화면에 보이는 컴포넌트를 바꾼다. 이미 3마리가 등록되어 있는 상태에서 등록하기 버튼 클릭 => alert 출력

--- src/pages/mypage/PetInfo.js
+++ src/pages/mypage/PetInfo.js
@@ -1,12 +1,40 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from 'react-router-dom';
 import NoContents from "../../components/NoContents";
 import "../../styles/StylePetInfo.css";
 import PetCard from "../../components/PetCard";
 import HeaderWithNav from '../../components/HeaderWithNav';
 import Footer from '../../components/Footer';
+import { data1, data2 } from '../Data';
 
 function PetInfo() {
+  const [petCards, setPetCards] = useState([
+    {
+      id: 1,
+      petImgSrc: "/imgs/dog3.jpeg",
+      petInfo: "똑바로/토이푸들/여아/12살/5kg",
+    },
+    {
+      id: 2,
+      petImgSrc: "/imgs/dog3.jpeg",
+      petInfo: "아지/말티즈/여아/12살/5kg",
+    },
+    {
+      id: 3,
+      petImgSrc: "/imgs/dog3.jpeg",
+      petInfo: "아지/말티즈/여아/12살/5kg",
+    },
+  ]);
+
+  const navigate = useNavigate();
+  const handleAddPetCard = () => {
+    if (petCards.length >= 3) {
+      alert("최대 3마리까지 등록할 수 있습니다.");
+    } else {
+      navigate('/petinfoadd');
+    }
+    setPetCards([...petCards]);
+  }
 
   return (
     <div>
@@ -26,19 +54,24 @@ function PetInfo() {
       <div className="mypage-navunderLine"></div>
 
       <div className="petinfo-container">
-        <button className="mypage-petinfo-add">
-          <Link to='/petInfoadd'
-                style={{ textDecoration: 'none', color: 'white' }}>
-            등록하기
-          </Link>
+        <button className="mypage-petinfo-add" onClick={handleAddPetCard}>
+          등록하기
         </button>
 
         <div className="pet-card-components">
-          <NoContents text="반려동물 정보"></NoContents>
-          <PetCard petImgSrc="/imgs/dog3.jpeg"
-                   petInfo="똑바로/토이푸들/여아/12살/5kg" />
-          <PetCard petImgSrc="/imgs/dog3.jpeg"
-                  petInfo="아지/말티즈/여아/12살/5kg" />
+          {data1.pet ? (
+            <>
+              {petCards.map((petCard) => (
+                <PetCard
+                  key={petCard.id}
+                  petImgSrc={petCard.petImgSrc}
+                  petInfo={petCard.petInfo}
+                />
+              ))}
+            </>
+          ) : (
+            <NoContents text="반려동물 정보" />
+          )}
         </div>
       </div>
 
