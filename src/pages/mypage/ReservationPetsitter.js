491c03028e18dacbe4b32691f3014d59b25ccc43
Jiyeong <tokyru@naver.com>
2024-02-13 17:14:23 +0900

    예약확인 컴포넌트 추가

--- src/pages/mypage/ReservationPetsitter.js
+++ src/pages/mypage/ReservationPetsitter.js
@@ -7,7 +7,7 @@ import ReservePetsitterCard from '../../components/ReservePetsitterCard';
 import Footer from '../../components/Footer';
 function ReservationPet() {
   const [checkedReservations, setCheckedReservations] = useState([false, false]);
-  const [reservationStates, setReservationStates] = useState(["승인대기", "예약완료"]);
+  const [reservationStates, setReservationStates] = useState(["예약확인","승인대기", "예약완료"]);
 
   const handleCheckboxClick = (index) => {
     setCheckedReservations((prevChecked) => {
@@ -28,6 +28,14 @@ function ReservationPet() {
           return updatedStates;
         });
       }
+    });
+  };
+
+  const handleConfirmReservation = (index) => {
+    setReservationStates((prevStates) => {
+      const updatedStates = [...prevStates];
+      updatedStates[index] = "승인대기";
+      return updatedStates;
     });
   };
 
@@ -69,7 +77,8 @@ function ReservationPet() {
             />
             <ReservePetsitterCard reservePetImgSrc="/imgs/dog3.jpeg"
                                   petInfo="똑바로 / 2023.12.23 ~ 2023.12.24 / 50,000원"
-                                  state={reservationStates[0]} />
+                                  state={reservationStates[0]}
+                                  onClick={() => handleConfirmReservation(0)} />
           </label>
           <label className="checked-width-2">
             <input
@@ -79,7 +88,19 @@ function ReservationPet() {
               onChange={() => handleCheckboxClick(1)} />
             <ReservePetsitterCard reservePetImgSrc="/imgs/dog3.jpeg"
                                   petInfo="뭉치 / 2024.01.11 16시 ~ 19시 / 20,000원"
-                                  state={reservationStates[1]} />
+                                  state={reservationStates[1]}
+                                  onClick={() => handleConfirmReservation(1)} />
+          </label>
+          <label className="checked-width-3">
+            <input
+              className="reservepet-checkbox"
+              type="checkbox"
+              checked={checkedReservations[3]}
+              onChange={() => handleCheckboxClick(2)} />
+            <ReservePetsitterCard reservePetImgSrc="/imgs/dog3.jpeg"
+                                  petInfo="뭉치 / 2024.01.11 16시 ~ 19시 / 20,000원"
+                                  state={reservationStates[2]}
+                                  onClick={() => handleConfirmReservation(2)} />
           </label>
 
           <NoContents text="반려동물 정보" />
