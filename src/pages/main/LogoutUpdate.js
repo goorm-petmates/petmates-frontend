a488edc3c7e8922ca107c985be8cebe64c049cc7
Jiyeong <tokyru@naver.com>
2024-02-15 16:26:13 +0900

    로그아웃 시 토큰 관리 코드 추가

---
+++ src/pages/main/LogoutUpdate.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const LogoutUpdate = () => {
+  const navigate = useNavigate(); // useNavigate 훅 사용
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/kakao/logout/update', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.result == 'success') {
+          console.log("카카오 토큰 갱신 완료")
+          console.log("카카오 로그아웃 재요청")
+          navigate('/logout');
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, [navigate]);
+};
+
+export default LogoutUpdate;
