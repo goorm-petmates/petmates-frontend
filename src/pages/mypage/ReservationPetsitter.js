178870dfd16460de36050f8ed5c40957aee1878c
Jiyeong <tokyru@naver.com>
2024-02-16 16:04:48 +0900

    주석처리

--- src/pages/mypage/ReservationPetsitter.js
+++ src/pages/mypage/ReservationPetsitter.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "../../styles/ReservaionPetsitter.css";
 import NoContents from "../../components/NoContents";
 import {Link} from "react-router-dom";
@@ -44,6 +44,22 @@ function ReservationPet() {
   if (data1.reservations_status === '') {
     return <NoContents text="예약 내역이 없습니다." />;
   }
+
+  // fetch('https://api.petmates.co.kr/api/members/test/api/reserve/{memberId}') // 요청을 보낼 URL을 지정합니다.
+  //   .then(response => {
+  //     if (!response.ok) {
+  //       throw new Error('네트워크 에러 발생'); // 오류 처리를 위해 HTTP 상태 코드를 확인합니다.
+  //     }
+  //     return response.json(); // JSON 형식의 응답을 파싱하여 Promise를 반환합니다.
+  //   })
+  //   .then(data => {
+  //     console.log(data); // JSON 데이터를 콘솔에 출력합니다.
+  //     // 여기서 데이터를 처리하거나 다른 작업을 수행할 수 있습니다.
+  //   })
+  //   .catch(error => {
+  //     console.error( error); // 오류가 발생하면 콘솔에 오류 메시지를 출력합니다.
+  //   });
+
 
   return (
     <div>
