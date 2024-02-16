a3db237693ef0efeac7bcdc09c734e5e2562fb12
Hyejung Na <hyejung.na01@gmail.com>
2024-02-16 16:27:55 +0900

    data1 내용 price, imgsrc수정

--- src/pages/Data.js
+++ src/pages/Data.js
@@ -1,13 +1,13 @@
 export const data1 = {
-  nickname: "test1",
-  email: "test1@gmail.com",
-  phone: "01012345678",
-  address: "서울 중구 세종대로 지하 2 (남대문로5가, 서울역 1호선)",
-  profile: "",
+  nickname: 'test1',
+  email: 'test1@gmail.com',
+  phone: '01012345678',
+  address: '서울 중구 세종대로 지하 2 (남대문로5가, 서울역 1호선)',
+  profile: '',
 
   pet: '2',
 
-  petImgSrc: "/imgs/dog3.jpeg",
+  petImgSrc: '/imgs/pet_img_1.png',
   name: '똑바로',
   breed: '푸들',
   sex: 'F',
@@ -18,26 +18,26 @@ export const data1 = {
   isDisease: false,
   etc: 'Friendly dog',
 
-  price: 50000,
+  price: 16500,
 
   reservations_status: 'Y',
   reservations_cancle: 'Y',
-  petsitterForm:'Y',
-  review:'Y',
+  petsitterForm: 'Y',
+  review: 'Y',
   review_status: '후기작성',
-  review_content:'감사합니다.',
+  review_content: '감사합니다.',
   review_star: 5,
-}
+};
 export const data2 = {
-  nickname: "test2",
-  email: "test2@gmail.com",
-  phone: "01045678901",
-  address: "서울 중구 세종대로 지하 2 (남대문로5가, 서울역 1호선)",
-  profile: "",
+  nickname: 'test2',
+  email: 'test2@gmail.com',
+  phone: '01045678901',
+  address: '서울 중구 세종대로 지하 2 (남대문로5가, 서울역 1호선)',
+  profile: '',
 
   pet: '2',
 
-  petImgSrc: "/imgs/dog3.jpeg",
+  petImgSrc: '/imgs/dog3.jpeg',
   name: '버디',
   breed: '골든리트리버',
   sex: 'M',
@@ -48,13 +48,12 @@ export const data2 = {
   isDisease: false,
   etc: 'Friendly dog',
 
-  price:100000,
+  price: 100000,
 
   reservations_status: 'Y',
-  petsitterForm:'Y',
-  review:'Y',
+  petsitterForm: 'Y',
+  review: 'Y',
   review_status: '작성완료',
-  review_content:'감사합니다.',
+  review_content: '감사합니다.',
   review_star: 5,
-}
-
+};
