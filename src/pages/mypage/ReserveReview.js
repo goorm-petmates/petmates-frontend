d0470e98335780eb3924bff68bdd0a107a1c48d0
jiyeong <tokyru@naver.com>
2024-02-27 11:28:55 +0900

    /api/petsitter/reviews로 후기 내역 불러오기 ==> contents, rating은 별도

--- src/pages/mypage/ReserveReview.js
+++ src/pages/mypage/ReserveReview.js
@@ -1,38 +1,48 @@
-import React, { useState } from 'react';
-import HeaderWithNav from '../../components/HeaderWithNav';
+import React, { useEffect, useState } from 'react';
 import Footer from '../../components/Footer';
 import { Link } from 'react-router-dom';
 import '../../styles/ReserveReview.css';
-import ReservePetsitterCard from '../../components/ReservePetsitterCard';
 import NoContents from '../../components/NoContents';
-import ReviewCard from '../../components/ReviewCard';
-// import { useMutation } from 'react-query';
 import Review from '../../components/Review';
-import { data1, data2 } from '../Data';
 
 const ReserveReview = () => {
-  // // 후기 가져오기
-  // const { data: completedReviews, isLoading, isError } = useQuery('completedReviews', async () => {
-  //   const response = await fetch('/api/petsitter/reviews');
-  //   if (!response.ok) {
-  //     throw new Error('Failed to fetch completed reviews');
-  //   }
-  //   return response.json();
-  // });
-
-  const [selectedCard, setSelectedCard] = useState(data1.review === 'Y' ? '후기작성' : 'N');
-  const [isReviewWritten, setIsReviewWritten] = useState(false);
-
-  const handleCardClick = (cardState) => {
-    setSelectedCard('작성완료');
-    setIsReviewWritten(false);
-  };
+  // const [selectedCard, setSelectedCard] = useState(data1.review === 'Y' ? '후기작성' : 'N');
+  // const [isReviewWritten, setIsReviewWritten] = useState(false);
+  //
+  // const handleCardClick = (cardState) => {
+  //   setSelectedCard('작성완료');
+  //   setIsReviewWritten(false);
+  // };
 
   const handleSaveReview = (rating, reviewText) => {
     // 후기 저장 로직을 구현합니다.
     console.log('별점:', rating);
     console.log('후기 내용:', reviewText);
   };
+
+  const [reviewCard, setReviewCard] = useState([]);
+
+  useEffect(() => {
+    fetch(`/api/petsitter/reviews`)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.data);
+
+        const formData = res.data.map((review) => ({
+          id: review.id,
+          reviewPic: review.reviewPic,
+          petInfo: `${review.reviewNickname} / ${review.startDate} ~ ${review.endDate} / ${review.totalPrice}원 `,
+          state: review.state,
+        }));
+
+        setReviewCard(formData);
+        //setCheckedReservations(Array(formData.length).fill(true));
+        //setReservationStates(Array(formData.length).fill('승인대기'));
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }, []);
 
   return (
     <>
@@ -50,35 +60,49 @@ const ReserveReview = () => {
         <button className='reservation-review-nav2'>취소내역</button>
       </Link>
       <Link to='/reservereview'>
-        <button className='reservation-review-nav3' onClick={() => handleCardClick('후기작성')}>
-          후기작성
-        </button>
+        {/*<button className='reservation-review-nav3' onClick={() => handleCardClick('후기작성')}>*/}
+        {/*  후기작성*/}
+        {/*</button>*/}
       </Link>
 
       <div className='mypage-navunderLine'></div>
 
       <div className='review-container'>
-        {data1.review === 'N' ? (
-          <NoContents text='후기 작성 내역' />
-        ) : (
-          <>
-            <Review
-              reviewImgSrc='/imgs/pet_img_1.png'
-              petInfo={`${data1.name} / 2024.02.16 ~ 2024.02.16 / ${data1.price}원`}
-              onSave={handleSaveReview}
-              state={selectedCard}
-              reviewContent={data1.review_content}
-              reviewStar={data1.review_star}
-            />
+        {/*{data1.review === 'N' ? (*/}
+        {/*  <NoContents text='후기 작성 내역' />*/}
+        {/*) : (*/}
+        {/*  <>*/}
+        {/*    <Review*/}
+        {/*      reviewImgSrc='/imgs/pet_img_1.png'*/}
+        {/*      petInfo={`${data1.name} / 2024.02.16 ~ 2024.02.16 / ${data1.price}원`}*/}
+        {/*      onSave={handleSaveReview}*/}
+        {/*      state={selectedCard}*/}
+        {/*      reviewContent={data1.review_content}*/}
+        {/*      reviewStar={data1.review_star}*/}
+        {/*    />*/}
 
-            <Review
-              reviewImgSrc='/imgs/dog3.jpeg'
-              petInfo={`${data2.name} / 2024.01.11 16시 ~ 19시 / ${data2.price}원`}
-              state={data2.review_status}
-              reviewContent={data2.review_content}
-              reviewStar={data2.review_star}
-            />
-          </>
+        {/*    <Review*/}
+        {/*      reviewImgSrc='/imgs/dog3.jpeg'*/}
+        {/*      petInfo={`${data2.name} / 2024.01.11 16시 ~ 19시 / ${data2.price}원`}*/}
+        {/*      state={data2.review_status}*/}
+        {/*      reviewContent={data2.review_content}*/}
+        {/*      reviewStar={data2.review_star}*/}
+        {/*    />*/}
+        {/*  </>*/}
+        {/*)}*/}
+
+        {reviewCard.length > 0 ? (
+          reviewCard.map((card) => (
+              <Review
+                reviewImgSrc={card.reviewPic}
+                petInfo={card.petInfo}
+                state={card.state}
+                reviewContent={card.reviewContents}
+                reviewStar={card.reviewRating}
+                onSave={handleSaveReview} />
+          ))
+        ) : (
+          <NoContents text='후기 내역이 없습니다.' />
         )}
       </div>
       <Footer />
