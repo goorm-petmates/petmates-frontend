99eaa36d39dff13a2efbe354a82e6d1cb9d02e5c
jiyeong <tokyru@naver.com>
2024-02-29 22:35:22 +0900

    useNavigate 추가

--- src/pages/main/TokenUpdate.js
+++ src/pages/main/TokenUpdate.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 import '../../styles/StyleSignUp.css';
 //import HeaderWithNav from '../../components/HeaderWithNav';
+import { useNavigate } from 'react-router-dom';
 
 const TokenUpdate = () => {
+  const navigate = useNavigate();
   useEffect(() => {
     const updateToken = async () => {
       // 백엔드 엔드포인트로 POST 요청 보내기
@@ -17,17 +19,20 @@ const TokenUpdate = () => {
 
         if (response.ok) {
           console.log('토큰 업데이트 완료'); // 갱신된 refreshToken 출력
+          navigate('/');
         } else {
           console.error('Token update failed:', response.status);
+          navigate('/');
           // 에러 처리 로직 (예: 로그아웃 처리)
         }
       } catch (error) {
         console.error('Error during token update:', error);
+        navigate('/');
       }
     };
 
     updateToken(); // 토큰 업데이트 함수 호출
-  }, []);
+  }, [navigate]);
 
   return null;
 };
