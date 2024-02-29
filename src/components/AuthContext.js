03508e3f7bd89b6be65251c4f5641741cc04f8d6
jiyeong <tokyru@naver.com>
2024-02-29 22:46:21 +0900

    https://petmates.co.kr/api/kakao/login/status 변경

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -11,7 +11,7 @@ export const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     // 로그인 상태 확인 API 호출
-    fetch('https://api.petmates.co.kr/api/members/test/api/kakao/login/status', {
+    fetch('https://petmates.co.kr/api/kakao/login/status', {
       method: 'GET',
       credentials: 'include',
     })
