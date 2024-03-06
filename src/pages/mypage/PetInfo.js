df185a01555740e53f5f5f48da2923d85599ea59
jiyeong <tokyru@naver.com>
2024-03-06 12:55:05 +0900

    data.result === "failed" 일때 추가

--- src/pages/mypage/PetInfo.js
+++ src/pages/mypage/PetInfo.js
@@ -10,32 +10,51 @@ function PetInfo() {
   const location = useLocation();
 
   const [petCards, setPetCards] = useState([]);
-
   const memberId = 1;
 
+  const [modalMessage, setModalMessage] = useState("");
+
    useEffect(() => {
-      // memberId를 사용하여 MSW 핸들러에서 반환된 응답을 사용
-      fetch(`https://petmates.co.kr/api/petinfo/${memberId}`)
-        .then((res) => res.json())
-        .then((res) => {
-          console.log(res.data);
+     fetch(`https://petmates.co.kr/api/oauth/jwt/check`)
+       .then((res) => {
+         if (!res.ok) {
+           throw new Error('Network response was not ok');
+         }
+         return res.json();
+       })
+       .then((data) => {
+         console.log(data);
+         if (data.result === "failed") {
+           navigate('/');
+           openModal();
+           setModalMessage("로그인 되어 있지 않아 메인 화면으로 이동합니다.");
+         }else {
+           // memberId를 사용하여 MSW 핸들러에서 반환된 응답을 사용
+           fetch(`https://petmates.co.kr/api/petinfo/${memberId}`)
+             .then((res) => res.json())
+             .then((res) => {
+               console.log(res.data);
 
-          // API 응답 데이터를 petCards 형태로 가공
-          const formattedData = res.data.map((pet) => ({
-            id: pet.id, // 반려동물의 고유 식별자
-            petImgSrc: pet.petImgSrc, // 반려동물 이미지 URL
-            petInfo: pet.name, // 반려동물 정보
-            startDate: pet.startDate,
-            endDate:pet.endDate,
-            price:pet.totalPrice,
-          }));
+               // API 응답 데이터를 petCards 형태로 가공
+               const formattedData = res.data.map((pet) => ({
+                 id: pet.id, // 반려동물의 고유 식별자
+                 petImgSrc: pet.petImgSrc, // 반려동물 이미지 URL
+                 petInfo: pet.name, // 반려동물 정보
+                 startDate: pet.startDate,
+                 endDate:pet.endDate,
+                 price:pet.totalPrice,
+               }));
 
-          setPetCards(formattedData);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-
+               setPetCards(formattedData);
+             })
+             .catch((error) => {
+               console.error(error);
+             });
+         }
+       })
+       .catch((error) => {
+         console.error('Error:', error);
+       });
    }, []);
 
 
@@ -48,10 +67,10 @@ function PetInfo() {
   const closeModal = () => {
     setShowModal(false);
   };
-  const modalMessage = "최대 3마리까지 등록할 수 있습니다.";
   const handleAddPetCard = () => {
     if (petCards.length >= 3) {
       openModal();
+      setModalMessage("최대 3마리까지 등록할 수 있습니다.")
     } else {
       navigate('/petinfoadd');
     }
