62bc5d72529a5c1c042da2bbc916088850389854
Hyejung Na <hyejung.na01@gmail.com>
2024-03-07 17:55:16 +0900

    목서버 사용 해제

--- src/index.js
+++ src/index.js
@@ -3,24 +3,24 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 //mswjs 사용시 주석해제하기
-async function deferRender() {
-  const { worker } = await import('./mocks/browser.js');
-  return worker.start();
-}
+// async function deferRender() {
+//   const { worker } = await import('./mocks/browser.js');
+//   return worker.start();
+// }
 
-deferRender().then(() => {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    // <React.StrictMode>
-    <App />,
-    // </React.StrictMode>,
-  );
-});
+// deferRender().then(() => {
+//   const root = ReactDOM.createRoot(document.getElementById('root'));
+//   root.render(
+//     // <React.StrictMode>
+//     <App />,
+//     // </React.StrictMode>,
+//   );
+// });
 
 //실제 api연결시 주석해제하기
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   // <React.StrictMode>
-//   <App />,
-//   // </React.StrictMode>,
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>,
+);
